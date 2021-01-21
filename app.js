b9b603d99de3466e9a61d94bd4bcc97fcd98a318
Grantjos12 <grantjoslyn@gmail.com>
2021-01-20 21:40:15 -0600

    background css complete

--- app.js
+++ app.js
@@ -1,3 +1,63 @@
-fetch("https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=daft_punk")
-  .then((response) => response.json())
-  .then((data) => console.log(data.album));
+const search = document.querySelector("#search");
+const searchBtn = document.querySelector("#search_btn");
+const output = document.querySelector("#output");
+
+//Artist search for loading data on page load
+let artist = "Led Zeppelin";
+
+window.onload = artistSearch();
+
+function artistSearch() {
+  fetch(`https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artist}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let albums = data.album;
+      console.log(albums);
+      let output = "";
+      albums.map((album) => {
+        output += `
+        
+        
+        
+        
+        
+        
+        `;
+      });
+    });
+}
+
+//Artist search on input click
+let artistInput = search.value;
+searchBtn.addEventListener("click", artistSearchTwo);
+
+function artistSearchTwo() {
+  fetch(
+    `https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artistInput}`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let albums = data.album;
+      console.log(albums);
+      let outputTwo = "";
+      albums.map((album) => {
+        output += `
+        ${album.strAlbum}
+        Year released: ${album.intYearReleased}
+        ${album.strAlbum}
+        ${album.strAlbum3DFace}
+        ${album.strDescriptionEN}
+        
+        
+        
+        
+        
+        `;
+      });
+      output.innerHTML = outputTwo;
+    });
+}
