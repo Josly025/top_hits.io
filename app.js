2b25e67b594341c4f9753c3e5696bd341c289f7a
Grantjos12 <grantjoslyn@gmail.com>
2021-01-21 21:39:31 -0600

    added music video cards

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const searchInput = document.querySelector("#search");
 const searchBtn = document.querySelector("#search_btn");
 const output = document.querySelector("#output");
-
+const outputVideo = document.querySelector("#outputVideo");
 //Artist search for loading data on page load
 let artist = "Led Zeppelin";
 
@@ -16,7 +16,7 @@ function artistSearch() {
     .then((data) => {
       let albums = data.album;
       console.log(albums);
-      // rendering and mapping through albums
+
       let albumStr = "";
       albums.map((album) => {
         albumStr += `
@@ -27,18 +27,39 @@ function artistSearch() {
         <img src="${album.strAlbumThumb}" alt="album">
         </div>
         `;
+        output.innerHTML = albumStr;
       });
-      output.innerHTML = albumStr;
+
+      const artistId = parseInt(albums[0].idArtist);
+      console.log(artistId);
+      fetch(`https://theaudiodb.com/api/v1/json/1/mvid.php?i=${artistId}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let videoData = data.mvids;
+          console.log(videoData);
+          let videoStr = "";
+          ////
+          videoData.map((video) => {
+            videoStr += `
+        <div class="card-video">
+        <h2>${video.strTrack}</h2>
+       
+      <a class="anchor" href="${video.strMusicVid}">Music Video</a>
+       
+      </div>
+        `;
+          });
+          output.innerHTML = videoStr;
+        });
     });
 }
 
 //Artist search on input click
-
-searchBtn.addEventListener("click", artistSearchTwo);
-
-function artistSearchTwo() {
+searchBtn.addEventListener("click", function artistSearchTwo(e) {
+  e.preventDefault();
   let artistInput = searchInput.value;
-
   fetch(
     `https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artistInput}`
   )
@@ -48,7 +69,6 @@ function artistSearchTwo() {
     })
     .then((data) => {
       let newAlbums = data.album;
-
       /// rendering and mapping through newAlbums
       let renderAlbums = "";
       newAlbums.map((album) => {
@@ -58,19 +78,19 @@ function artistSearchTwo() {
         <h2>${album.strAlbum}</h2>
         <h3>Year released: ${album.intYearReleased}</h3>
         <img src="${album.strAlbumThumb}" alt="album">
+
         </div>
-         
         `;
       });
       output.innerHTML = renderAlbums;
     });
 
-  // setTimeout(() => {
-  //   clearInput();
-  // }, 10000);
-}
+  setTimeout(() => {
+    clearInput();
+  }, 10000);
+});
 
 //clear the input after a search
-// function clearInput() {
-//   searchInput.value = "";
-// }
+function clearInput() {
+  searchInput.value = "";
+}
