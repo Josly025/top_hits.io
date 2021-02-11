79c489586163674ced48f4cc5bb0846fcda8efc8
Grantjos12 <grantjoslyn@gmail.com>
2021-02-11 11:00:11 -0600

    added new api call

--- app.js
+++ app.js
@@ -9,101 +9,106 @@ window.onload = artistSearch();
 
 function artistSearch() {
   let initalAlbums = "";
-  fetch(`https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artist}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      let albums = data.album;
-      console.log(albums);
 
-      let albumStr = "";
-      albums.map((album) => {
-        albumStr += `
+  const apiCall = {
+    method: "GET",
+    url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+    params: { q: `${artist}` },
+    headers: {
+      "x-rapidapi-key": "cb5b1f2f44msh3ccf1d2e09978fap1363abjsn0c69cbf92586",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+  axios.request(apiCall).then(function (response) {
+    console.log(response.data.data);
+    let albums = response.data.data;
+    let albumStr = "";
+    albums.map((album) => {
+      albumStr += `
         <div class="card">
-        <h1>${album.strArtist}</h1>
-        <h2>${album.strAlbum}</h2>
-        <h3>Year released: ${album.intYearReleased}</h3>
-        <img src="${album.strAlbumThumb}" alt="album">
+        <h1>${album.artist.name}</h1>
+        <h2>${album.title}</h2>
+        <h3>Song: ${album.title_short}</h3>
+        <img src="${album.album.cover_medium}" alt="album">
         </div>
         `;
-        output.innerHTML = albumStr;
-      });
+      output.innerHTML = albumStr;
+    });
 
-      const artistId = parseInt(albums[0].idArtist);
-      console.log(artistId);
-      fetch(`https://theaudiodb.com/api/v1/json/1/mvid.php?i=${artistId}`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          let videoData = data.mvids;
-          console.log(videoData);
-          let videoStr = "";
-          ////
-          videoData.map((video) => {
-            videoStr += `
+    const artistId = parseInt(albums[0].idArtist);
+    console.log(artistId);
+    fetch(`theaudiodb.com/api/v1/json/{APIKEY}/artist.php?i=${artistId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let videoData = data.mvids;
+        console.log(videoData);
+        let videoStr = "";
+        ////
+        videoData.map((video) => {
+          videoStr += `
         <div class="card-video">
         <h2>${video.strTrack}</h2>
       <a class="anchor" href="${video.strMusicVid}">Music Video</a>
       </div>
         `;
-          });
-          outputVideo.innerHTML = videoStr;
         });
-    });
+        outputVideo.innerHTML = videoStr;
+      });
+  });
 }
 
 //Artist search on input click
 searchBtn.addEventListener("click", function artistSearchTwo(e) {
-  e.preventDefault();
   let artistInput = searchInput.value;
-  fetch(
-    `https://www.theaudiodb.com/api/v1/json/1/searchalbum.php?s=${artistInput}`
-  )
-    .then((response) => {
-      return response.json();
-      console.log(response.json() + "this is response");
-    })
-    .then((data) => {
-      let newAlbums = data.album;
-      /// rendering and mapping through newAlbums
-      let renderAlbums = "";
-      newAlbums.map((album) => {
-        renderAlbums += `
-        <div class="card">
-        <h1>${album.strArtist}</h1>
-        <h2>${album.strAlbum}</h2>
-        <h3>Year released: ${album.intYearReleased}</h3>
-        <img src="${album.strAlbumThumb}" alt="album">
+  const searchCall = {
+    method: "GET",
+    url: "https://deezerdevs-deezer.p.rapidapi.com/search",
+    params: { q: `${artistInput}` },
+    headers: {
+      "x-rapidapi-key": "cb5b1f2f44msh3ccf1d2e09978fap1363abjsn0c69cbf92586",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  };
+  axios.request(searchCall).then(function (response) {
+    let newAlbums = response.data.data;
 
+    let renderAlbums = "";
+    newAlbums.map((album) => {
+      renderAlbums += `
+         <div class="card">
+        <h1>${album.artist.name}</h1>
+        <h2>${album.title}</h2>
+        <h3>Song: ${album.title_short}</h3>
+        <img src="${album.album.cover_medium}" alt="album">
         </div>
         `;
-      });
-      output.innerHTML = renderAlbums;
-      const artistTwo = parseInt(newAlbums[0].idArtist);
-      console.log(artistTwo);
-      fetch(`https://theaudiodb.com/api/v1/json/1/mvid.php?i=${artistTwo}`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          let videoDataTwo = data.mvids;
-          console.log(videoDataTwo);
-          let videoStrTwo = "";
-          ////
-          videoDataTwo.map((video) => {
-            videoStrTwo += `
+    });
+    output.innerHTML = renderAlbums;
+    const artistTwo = parseInt(newAlbums[0].idArtist);
+    console.log(artistTwo);
+    fetch(`https://theaudiodb.com/api/v1/json/1/mvid.php?i=${artistTwo}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let videoDataTwo = data.mvids;
+        console.log(videoDataTwo);
+        let videoStrTwo = "";
+        ////
+        videoDataTwo.map((video) => {
+          videoStrTwo += `
         <div class="card-video">
         <h2>${video.strTrack}</h2>
       <a class="anchor" href="${video.strMusicVid}">Music Video</a>
       </div>
         `;
-          });
-          outputVideo.innerHTML = videoStrTwo;
         });
-    });
-
+        outputVideo.innerHTML = videoStrTwo;
+      });
+  });
+  e.preventDefault();
   setTimeout(() => {
     clearInput();
   }, 10000);
